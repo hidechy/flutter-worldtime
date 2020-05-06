929cdaa67a5c155260be94d70c393cf1177ed351
Hide Toyoda <hide.toyoda@gmail.com>
2020-05-06 22:28:28 +0900

    flutter ninja 30 finish

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,8 +13,10 @@ class _LoadingState extends State<Loading> {
     WorldTime instance = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
     });
   }
 
