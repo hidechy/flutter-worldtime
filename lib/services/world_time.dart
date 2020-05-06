30e30c1b72b76bca3537a9b545c320481e3b4669
Hide Toyoda <hide.toyoda@gmail.com>
2020-05-06 22:37:36 +0900

    flutter ninja 31 finish

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location; // location name for UI
@@ -12,7 +13,7 @@ class WorldTime {
   Future<void> getTime() async {
     try {
       Response response =
-          await get('http://worldtimeapi.org/api/timezone/$url');
+      await get('http://worldtimeapi.org/api/timezone/$url');
       Map data = jsonDecode(response.body);
       String datetime = data['datetime'];
       String offset = data['utc_offset'].substring(1, 3);
@@ -20,7 +21,7 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
 
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     } catch (e) {
       print('caught error: $e');
       time = 'could not get time data.';
